41508fbbbf1604c8d3b13422864cebf652d25560
Paul Dunkirk <pauldunkirk@gmail.com>
2017-01-25 14:25:31 -0600

    first commit

---
+++ ObjectAssessment.js
@@ -0,0 +1,14 @@
+
+
+var avengersMovie = {
+	name: "Avengers",
+	runtimeInMins: 143,
+	releaseYear: 2012,
+	director: "Joss Whedon"
+};
+console.log(avengersMovie);
+
+
+var clicked = function () {
+		console.log(avengersMovie);
+};
